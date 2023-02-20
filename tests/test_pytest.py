607365dee3bcbee23d2bd990ac69b33ac9cfa77a
Jurian Beunk <jurianbeunk@gmail.com>
2023-02-20 17:13:54 +0100

    update

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -7,12 +7,12 @@ class TestClass:
     def test_get_reservoirs(skip=50, limit=2):
         # test if reservoir is retrieved and has the expected id
         r = cli.get_reservoirs(skip=50, limit=2)
-        gdf = utils.to_geopandas(r.json())
+        gdf = utils.to_geopandas(r)
         assert gdf.iloc[0]["id"] == 60
 
     def test_get_reservoir_ts(id=3001):
         # test if reservoir ts is retreived and has expected column header
-        assert cli.get_reservoir_ts(3001).json()[0]['name'] == 'surface_water_area'
+        assert cli.get_reservoir_ts(3001)[0]['name'] == 'surface_water_area'
 
     def test_get_reservoirs_by_geom(geom):
         
